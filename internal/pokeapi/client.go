78de350a85e94e512fd46ccc1169b9b25eb2f1d1
Ellie <github@lly.email>
2025-08-10 11:08:46 -0500

    fix: fixed logic typo in client

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -10,16 +10,9 @@ type Client struct {
 }
 
 func NewClient(timeout time.Duration) *Client {
-	if timeout < 1 {
-		return &Client{
-			HTTP: http.Client{
-				Timeout: 0,
-			},
-		}
-	}
 	return &Client{
 		HTTP: http.Client{
-			Timeout: timeout * time.Second,
+			Timeout: timeout,
 		},
 	}
 }
